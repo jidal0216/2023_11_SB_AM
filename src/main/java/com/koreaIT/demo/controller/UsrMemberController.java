9e3f6873331d353025accbbcfd3ac4b90c366056
jidal0216 <43128624+jidal0216@users.noreply.github.com>
2023-11-13 14:56:07 +0900

    result data 적용

---
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -0,0 +1,56 @@
+package com.koreaIT.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.koreaIT.demo.service.MemberService;
+import com.koreaIT.demo.util.Util;
+import com.koreaIT.demo.vo.Member;
+import com.koreaIT.demo.vo.ResultData;
+
+@Controller
+public class UsrMemberController {
+	
+	private MemberService memberService;
+	
+	UsrMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		
+		if (Util.empty(loginId)) {
+			return ResultData.from("F-1", "아이디를 입력해주세요");
+		}
+		if (Util.empty(loginPw)) {
+			return ResultData.from("F-2", "비밀번호를 입력해주세요");
+		}
+		if (Util.empty(name)) {
+			return ResultData.from("F-3", "이름을 입력해주세요");
+		}
+		if (Util.empty(nickname)) {
+			return ResultData.from("F-4", "닉네임을 입력해주세요");
+		}
+		if (Util.empty(cellphoneNum)) {
+			return ResultData.from("F-5", "전화번호를 입력해주세요");
+		}
+		if (Util.empty(email)) {
+			return ResultData.from("F-6", "이메일을 입력해주세요");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if (member != null) {
+			return ResultData.from("F-7", Util.f("이미 사용중인 아이디(%s) 입니다", loginId));
+		}
+		
+		memberService.joinMember(loginId, loginPw, name, nickname, cellphoneNum, email);
+		
+		int id = memberService.getLastInsertId();
+		
+		return ResultData.from("S-1", "회원 가입 성공", memberService.getMemberById(id));
+	}
+}
---
+++ src/main/java/com/koreaIT/demo/util/Util.java
@@ -0,0 +1,17 @@
+package com.koreaIT.demo.util;
+
+public class Util {
+	public static boolean empty(String str) {
+		
+		if (str == null) {
+			return true;
+		}
+		
+		return str.trim().length() == 0;
+	}
+
+	public static String f(String format, Object... args) {
+		return String.format(format, args);
+	}
+	
+}
