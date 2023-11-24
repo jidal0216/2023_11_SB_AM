8e63606d743b6fbdbd68b6804a595a7990f84d6b
jidal0216 <43128624+jidal0216@users.noreply.github.com>
2023-11-24 16:45:49 +0900

    reply modify

--- src/main/java/com/koreaIT/demo/dao/ReplyDao.java
+++ src/main/java/com/koreaIT/demo/dao/ReplyDao.java
@@ -2,9 +2,11 @@ package com.koreaIT.demo.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.koreaIT.demo.vo.Reply;
 
@@ -35,4 +37,33 @@ public interface ReplyDao {
 			""")
 	List<Reply> getReplies(String relTypeCode, int relId);
 	
+	@Select("""
+			SELECT R.*, M.nickname AS writerName
+				FROM reply AS R
+				INNER JOIN `member` AS M
+				ON R.memberId = M.id
+				WHERE R.id = #{id}
+			""")
+	Reply forPrintReply(int id);
+	
+	@Select("""
+			SELECT *
+				FROM reply
+				WHERE id = #{id}
+			""")
+	Reply getReplyById(int id);
+
+	@Update("""
+			UPDATE reply
+				SET updateDate = NOW()
+					, `body` = #{body}
+				WHERE id = #{id}
+			""")
+	void modifyReply(int id, String body);
+	
+	@Delete("""
+			DELETE FROM reply
+				WHERE id = #{id}
+			""")
+	void deleteReply(int id);
 }
